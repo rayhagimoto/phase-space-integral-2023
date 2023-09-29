002550902509b2729f491fa2e27d218dc3441330
Ray <ray.m.hagimoto@gmail.com>
2023-09-29 11:16:20 -0500

    calc emissivity for all channels, not just ep

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def calc_emissivity(
         f"mb = {mb}\n"
         f"m1 = {m1}\n"
         f"m2 = {m2}\n"
-        # f"m3 = {m3}\n"
+        f"m3 = {m3}\n"
         f"pFa = {pFa}\n"
         f"pFb = {pFb}\n"
         f"pF1 = {pF1}\n"
@@ -84,11 +84,12 @@ def calc_emissivity(
         f"mu_b = {mu_b}\n"
         f"mu_1 = {mu_1}\n"
         f"mu_2 = {mu_2}\n"
-        f"neval = {neval}"
+        # f"beta_F_mu = {beta_F_mu}\n"
+        f"neval = {neval}\n"
+        f"n = {n}"
     )
 
-    print(f"mu +/- {n} * T\n")
-    print(f"T = {T}")
+    print(f"beta_F_mu = {beta_F_mu}")
     print(f"Parameters\n----------\n{params_str}")
 
     # ------------------- COMPUTE THE INTEGRAL ------------------- #
@@ -97,18 +98,13 @@ def calc_emissivity(
     # For constant matrix element, set conversion factor to 1 for convenience.
     # For full matrix element set conversion factor to  CONVERSION_FACTOR.
     f = Integrand(
-        ma,
-        mb,
-        m1,
-        m2,
+        process,
+        beta_F_mu,
         m3,
-        mu_a,
-        mu_b,
-        mu_1,
-        mu_2,
         T,
-        conversion_factor=CONVERSION_FACTOR,
+        CONVERSION_FACTOR,
     )
+
     # step 1 -- adapt to f; discard results
     integ(f, nitn=20, neval=neval, alpha=0.3)
 
@@ -126,10 +122,10 @@ def calc_emissivity(
         from datetime import date
 
         today = date.today().strftime("%d-%m-%Y")
-        filename = filename + f"-neval={neval}-{today}.csv"
+        filename = filename + f"-neval={neval}-1.csv"
         filepath = directory + "/" + filename
-        HEADER = params_str + "\n" + f"Columns:m3,emissivity (ergs/cm^3/s),error"
-        data = np.array([[m3, result.mean, result.sdev]])
+        HEADER = params_str + "\n" + f"Columns:beta_F_mu,emissivity (ergs/cm^3/s),error"
+        data = np.array([[beta_F_mu, result.mean, result.sdev]])
 
         save_file(filepath, header=HEADER, data=data)
         print(f"file updated at {filepath}")
@@ -138,22 +134,45 @@ def calc_emissivity(
 
 
 def main():
+    m3 = 0
     process = DEFAULT_VALUES["process"]
     beta_F_mu = DEFAULT_VALUES["beta_F_mu"]
     T0 = DEFAULT_VALUES["T"]  # MeV
+    process_lists = [
+        ["ue->eea"],
+        # ["ep->upa", "up->epa", "ee->uea", "ue->eea"],
+        ["uu->eua", "eu->uua"],
+    ]
 
+    beta_F_mu_vals_list = [
+        np.hstack(
+            (
+                np.linspace(0.0, 0.1, 5),
+                np.linspace(0.1, 0.85, 6)[1:],
+                np.linspace(0.85, 0.95, 6)[1:],
+            )
+        ),
+        np.hstack((np.linspace(0.35, 0.4, 5), np.linspace(0.4, 0.95, 11)[1:])),
+    ]
 
-    for m3 in [0.01, 0.1, 1.]:
-        if m3 > 0: FILE_DIRECTORY = f"./results/massive-axion/{'-to-'.join(process.split('->'))}"
-        else: FILE_DIRECTORY = f"./results/{'-to-'.join(process.split('->'))}"
-        FILE_NAME = "m3-vs-emissivity"
+    for process_list, beta_F_mu_vals in zip(process_lists, beta_F_mu_vals_list):
+        for process in process_list:
+            print(f"\n--------\nStarting {process}\n--------")
+            for beta_F_mu in beta_F_mu_vals:
+                if m3 > 0:
+                    FILE_DIRECTORY = (
+                        f"./results/massive-axion/{'-to-'.join(process.split('->'))}"
+                    )
+                else:
+                    FILE_DIRECTORY = f"./results/{'-to-'.join(process.split('->'))}"
+                FILE_NAME = "beta_F_mu-vs-emissivity"
 
-        Path(FILE_DIRECTORY).mkdir(exist_ok=True, parents=True)
+                Path(FILE_DIRECTORY).mkdir(exist_ok=True, parents=True)
 
-        # for T in np.logspace(-3, 4, 50) * T0:
-        res = calc_emissivity(
-            process, beta_F_mu, T0, m3, FILE_DIRECTORY, FILE_NAME, save=True
-        )
+                # for T in np.logspace(-3, 4, 50) * T0:
+                res = calc_emissivity(
+                    process, beta_F_mu, T0, m3, FILE_DIRECTORY, FILE_NAME, save=True
+                )
 
 
 if __name__ == "__main__":
