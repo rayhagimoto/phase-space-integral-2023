8ebb2080d1e97a488dd02a980380f9144b8a1519
Ray <ray.m.hagimoto@gmail.com>
2023-09-29 16:38:10 -0500

    added symmetry factor

--- integrand.py
+++ integrand.py
@@ -23,7 +23,8 @@ given by the following integral, for which this module provides methods
 for computing the integrand. Note that all observables are measured in
 the rest frame of the neutron star.
 
-            Ɛ = (4 π) ∫   fa * fb * (1 - f1) * (1 - f2) 
+            Ɛ = (4 π) / S *
+                ∫  fa * fb * (1 - f1) * (1 - f2) 
                     * E3
                     * |M.E|^2 
                     * Sqrt(Ea^2 - ma^2) 
@@ -82,6 +83,11 @@ class Integrand:
 
         pFa, pFb, pF1, mu_a, mu_b, mu_1, mu_2 = get_fermi_params(process, beta_F_mu)
 
+        if process.split("->")[0] in ["ep", "up"]:
+            self.symmetry_factor = 1
+        else:
+            self.symmetry_factor = 2
+
         if m3 == 0:
             self.integrand = partial(
                 integrand_massless_axion,
@@ -117,16 +123,20 @@ class Integrand:
         self.conversion_factor = conversion_factor
 
     def __call__(self, x):
-        return self.conversion_factor * self.integrand(
-            Ea=x[0],
-            Eb=x[1],
-            E1=x[2],
-            cosa=x[3],
-            cosb=x[4],
-            cos1=x[5],
-            phia=x[6],
-            phib=x[7],
-            phi1=x[8],
+        return (
+            self.conversion_factor
+            * self.integrand(
+                Ea=x[0],
+                Eb=x[1],
+                E1=x[2],
+                cosa=x[3],
+                cosb=x[4],
+                cos1=x[5],
+                phia=x[6],
+                phib=x[7],
+                phi1=x[8],
+            )
+            / self.symmetry_factor
         )
 
 
@@ -382,128 +392,133 @@ def integrand_massless_axion(
         return _integrand
     return np.float64(0.0)
 
-# @jit(nopython=True, error_model="numpy")
-# def integrand_massless_axion(
-#     Ea,
-#     Eb,
-#     E1,
-#     cosa,
-#     cosb,
-#     cos1,
-#     phia,
-#     phib,
-#     phi1,
-#     ma,
-#     mb,
-#     m1,
-#     m2,
-#     mu_a,
-#     mu_b,
-#     mu_1,
-#     mu_2,
-#     T,
-#     matrix_element_sq,
-# ):
-#     """Integrand, including measure factors. Only valid for m3 = 0.
 
-#     Below 'NS frame' means neutron star rest frame.
+@jit(nopython=True, error_model="numpy")
+def integrand_massless_axion_approximate_p3mag(
+    Ea,
+    Eb,
+    E1,
+    cosa,
+    cosb,
+    cos1,
+    phia,
+    phib,
+    phi1,
+    ma,
+    mb,
+    m1,
+    m2,
+    mu_a,
+    mu_b,
+    mu_1,
+    mu_2,
+    T,
+    matrix_element_sq,
+):
+    """Integrand, including measure factors. Only valid for m3 = 0.
 
-#     Parameters
-#     ----------
-#         Ea : float
-#             Energy of ptle 'a' in NS frame.
-#         Eb : float
-#             Energy of ptle 'b' in NS frame.
-#         E1 : float
-#             Energy of ptle '1' in NS frame.
-#         cosa : float
-#             Polar angle of ptle 'a' measured relative to ptle '3' axis.
-#         cosb : float
-#             Polar angle of ptle 'b' measured relative to ptle '3' axis.
-#         cos1 : float
-#             Polar angle of ptle '1' measured relative to ptle '3' axis.
-#         phia : float
-#             Azimuthal angle of ptle 'a' measured relative to ptle '3' axis.
-#         phib : float
-#             Azimuthal angle of ptle 'b' measured relative to ptle '3' axis.
-#         phi1 : float
-#             Azimuthal angle of ptle '1' measured relative to ptle '3' axis.
-#         ma : float
-#             Mass of ptle 'a'.
-#         mb : float
-#             Mass of ptle 'b'.
-#         m1 : float
-#             Mass of ptle '1'.
-#         m2 : float
-#             Mass of ptle '2'.
-#         mu_a : float
-#             Chemical potential of ptle 'a'. Approximated by its Fermi Energy.
-#         mu_b : float
-#             Chemical potential of ptle 'b'. Approximated by its Fermi Energy.
-#         mu_1 : float
-#             Chemical potential of ptle '1'. Approximated by its Fermi Energy.
-#         mu_2 : float
-#             Chemical potential of ptle '2'. Approximated by its Fermi Energy.
-#         T : float
-#             Temperature assuming all speciies in thermal equilibrium.
-#     """
-#     norm = (4 * np.pi) / 2**5 / (2 * np.pi) ** 11
+    This method differs from integrand_massless_axion by approximating
+    p3mag by doing a series expansion of E2(p3mag) assuming p3mag / Epsilon
+    is small where Epsilon = sqrt(| pavec + pbvec - p1vec |^2 + m2^2).
 
-#     # ------ MOMENTUM RECONSTRUCTION ------ #
+    Below 'NS frame' means neutron star rest frame.
 
-#     # First calculate E3.
-#     # If it's positive, then this point in phase space is
-#     # kinematically allowed, so continue.
-#     sina = (1 - cosa**2) ** 0.5
-#     sinb = (1 - cosb**2) ** 0.5
-#     sin1 = (1 - cos1**2) ** 0.5
+    Parameters
+    ----------
+        Ea : float
+            Energy of ptle 'a' in NS frame.
+        Eb : float
+            Energy of ptle 'b' in NS frame.
+        E1 : float
+            Energy of ptle '1' in NS frame.
+        cosa : float
+            Polar angle of ptle 'a' measured relative to ptle '3' axis.
+        cosb : float
+            Polar angle of ptle 'b' measured relative to ptle '3' axis.
+        cos1 : float
+            Polar angle of ptle '1' measured relative to ptle '3' axis.
+        phia : float
+            Azimuthal angle of ptle 'a' measured relative to ptle '3' axis.
+        phib : float
+            Azimuthal angle of ptle 'b' measured relative to ptle '3' axis.
+        phi1 : float
+            Azimuthal angle of ptle '1' measured relative to ptle '3' axis.
+        ma : float
+            Mass of ptle 'a'.
+        mb : float
+            Mass of ptle 'b'.
+        m1 : float
+            Mass of ptle '1'.
+        m2 : float
+            Mass of ptle '2'.
+        mu_a : float
+            Chemical potential of ptle 'a'. Approximated by its Fermi Energy.
+        mu_b : float
+            Chemical potential of ptle 'b'. Approximated by its Fermi Energy.
+        mu_1 : float
+            Chemical potential of ptle '1'. Approximated by its Fermi Energy.
+        mu_2 : float
+            Chemical potential of ptle '2'. Approximated by its Fermi Energy.
+        T : float
+            Temperature assuming all speciies in thermal equilibrium.
+    """
+    norm = (4 * np.pi) / 2**5 / (2 * np.pi) ** 11
 
-#     pamag = (Ea**2 - ma**2) ** 0.5
-#     pbmag = (Eb**2 - mb**2) ** 0.5
-#     p1mag = (E1**2 - m1**2) ** 0.5
+    # ------ MOMENTUM RECONSTRUCTION ------ #
 
-#     pavec = pamag * np.array([sina * cos(phia), sina * sin(phia), cosa])
-#     pbvec = pbmag * np.array([sinb * cos(phib), sinb * sin(phib), cosb])
-#     p1vec = p1mag * np.array([sin1 * cos(phi1), sin1 * sin(phi1), cos1])
+    # First calculate E3.
+    # If it's positive, then this point in phase space is
+    # kinematically allowed, so continue.
+    sina = (1 - cosa**2) ** 0.5
+    sinb = (1 - cosb**2) ** 0.5
+    sin1 = (1 - cos1**2) ** 0.5
 
-#     Pvec = pavec + pbvec - p1vec
-#     Pmag = np_norm(Pvec)
-#     Epsilon = (Pmag**2 + m2**2) ** 0.5
-#     Pz = Pvec[2]
+    pamag = (Ea**2 - ma**2) ** 0.5
+    pbmag = (Eb**2 - mb**2) ** 0.5
+    p1mag = (E1**2 - m1**2) ** 0.5
 
-#     # E3 = p3mag since axion is massless
-#     p3mag = ((Ea + Eb - E1) - Epsilon) / (1 - Pz / Epsilon)
-#     E3 = p3mag
+    pavec = pamag * np.array([sina * cos(phia), sina * sin(phia), cosa])
+    pbvec = pbmag * np.array([sinb * cos(phib), sinb * sin(phib), cosb])
+    p1vec = p1mag * np.array([sin1 * cos(phi1), sin1 * sin(phi1), cos1])
 
-#     if E3 > 0:
-#         # jac_factor arises from using energy Dirac delta to fix
-#         # E3 to the value specified above.
-#         jac_factor = 1 / abs(
-#             1 + (E3 - Pz) / np.sqrt(Pmag**2 + m2**2 + E3**2 - 2 * E3 * Pz)
-#         )
+    Pvec = pavec + pbvec - p1vec
+    Pmag = np_norm(Pvec)
+    Epsilon = (Pmag**2 + m2**2) ** 0.5
+    Pz = Pvec[2]
 
-#         # Coord. system is chosen so that p3 points in z-direction.
-#         p3vec = E3 * np.array([0, 0, 1])
-#         p2vec = pavec + pbvec - p1vec - p3vec
-#         E2 = (np_norm(p2vec) ** 2 + m2**2) ** 0.5
+    # E3 = p3mag since axion is massless
+    p3mag = ((Ea + Eb - E1) - Epsilon) / (1 - Pz / Epsilon)
+    E3 = p3mag
 
-#         # ----- THE INTEGRAND ----- #
-#         _integrand = (
-#             norm
-#             * pamag
-#             * pbmag
-#             * p1mag
-#             * E3
-#             * jac_factor
-#             * E3
-#             / E2
-#             * thermal_factors(Ea, Eb, E1, E2, mu_a, mu_b, mu_1, mu_2, T)
-#             * matrix_element_sq(
-#                 Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
-#             )
-#         )
-#         return _integrand
-#     return np.float64(0.0)
+    if E3 > 0:
+        # jac_factor arises from using energy Dirac delta to fix
+        # E3 to the value specified above.
+        jac_factor = 1 / abs(
+            1 + (E3 - Pz) / np.sqrt(Pmag**2 + m2**2 + E3**2 - 2 * E3 * Pz)
+        )
+
+        # Coord. system is chosen so that p3 points in z-direction.
+        p3vec = E3 * np.array([0, 0, 1])
+        p2vec = pavec + pbvec - p1vec - p3vec
+        E2 = (np_norm(p2vec) ** 2 + m2**2) ** 0.5
+
+        # ----- THE INTEGRAND ----- #
+        _integrand = (
+            norm
+            * pamag
+            * pbmag
+            * p1mag
+            * E3
+            * jac_factor
+            * E3
+            / E2
+            * thermal_factors(Ea, Eb, E1, E2, mu_a, mu_b, mu_1, mu_2, T)
+            * matrix_element_sq(
+                Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
+            )
+        )
+        return _integrand
+    return np.float64(0.0)
 
 
 @jit(nopython=True, error_model="numpy")
@@ -581,13 +596,10 @@ def _fetch_matrix_element_sq(process, m3):
     in_state = process.split("->")[0]
     if m3 == 0:
         if in_state in ["ep", "up"]:
-            print(f"{in_state} --> lp_matrix_element_sq_massless")
             return lp_matrix_element_sq_massless
         if in_state in ["ee", "uu"]:
-            print(f"{in_state} --> ll_matrix_element_sq_massless")
             return ll_matrix_element_sq_massless
         if in_state in ["eu", "ue"]:
-            print(f"{in_state} --> llp_matrix_element_sq_massless")
             return llp_matrix_element_sq_massless
     return
 
@@ -619,15 +631,15 @@ def ll_matrix_element_sq_massless(
     Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
 ):
     """Calculate spin-summed matrix element squared for ll -> l'la processes."""
-
+    # same as (lp)
     lp_me_sq = lp_matrix_element_sq_massless(
         Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
     )
-
+    # (lp) but with a <-->  b
     permuted_me_sq = lp_matrix_element_sq_massless(
         Eb, Ea, E1, E2, E3, pbvec, pavec, p1vec, p2vec, p3vec, mb, ma, m1, m2
     )
-
+    # interaction term
     T_ll = ll_interaction_massless(
         Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
     )
@@ -640,15 +652,15 @@ def llp_matrix_element_sq_massless(
     Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
 ):
     """Calculate spin-summed matrix element squared for ll -> l'la processes."""
-
+    # same as (lp)
     lp_me_sq = lp_matrix_element_sq_massless(
         Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
     )
-
+    # (lp) but with 1 <-->  2
     permuted_me_sq = lp_matrix_element_sq_massless(
         Ea, Eb, E2, E1, E3, pavec, pbvec, p2vec, p1vec, p3vec, ma, mb, m2, m1
     )
-
+    # interaction term
     T_llp = llp_interaction_massless(
         Ea, Eb, E1, E2, E3, pavec, pbvec, p1vec, p2vec, p3vec, ma, mb, m1, m2
     )
