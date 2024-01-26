1a2a880e745931b88b99fba1650f1c1f1267d668
Ray H <37245986+rayhagimoto@users.noreply.github.com>
2024-01-26 11:10:01 -0600

    Update integrand.py

--- integrand.py
+++ integrand.py
@@ -60,7 +60,7 @@ as input. E.g.
 
     f = Integrand(process, beta_F_mu, m3, T). 
     x = [x0, x1, x2, x3, x4, x5, x6, x7, x8]
-    f(x) --> some number
+    # f(x) --> some number
 
 When f is initialised, it uses the process, beta_F_mu, m3, and T to
 uniquely determine all relevant parameters such as the masses 
